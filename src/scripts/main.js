44edae4a66c684672f99b43de5cffcb1dd4bd680
Nazar Kundys <nazarrisd@gmail.com>
2025-12-24 14:29:54 +0200

    fix: 🛠️ improved population calculation accuracy and data filtering
    - Added filter to remove **NaN** values from population data parsing
    - Applied `Math.trunc` to average population calculation for integer result

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const locale = 'en-US';
-const populations = [...document.getElementsByClassName('population')].map(
-  (el) => Number.parseInt(el.textContent.replaceAll(',', '')),
-);
+const populations = [...document.getElementsByClassName('population')]
+  .map((el) => Number.parseInt(el.textContent.replaceAll(',', '')))
+  .filter((num) => !isNaN(num));
 
 const totalPopulation = populations.reduce((acc, el) => acc + el);
 const totalPopulationEl = document.querySelector('.total-population');
 
 totalPopulationEl.textContent = totalPopulation.toLocaleString(locale);
 
-const avgPopulation = totalPopulation / populations.length;
+const avgPopulation = Math.trunc(totalPopulation / populations.length);
 const avgPopulationEl = document.querySelector('.average-population');
 
 avgPopulationEl.textContent = avgPopulation.toLocaleString('en-US');
